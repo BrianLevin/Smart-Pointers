c7e8297ac1041e7c1347bd3eed24aa4e7aac0067
BrianLevin <bml201095@gmail.com>
2021-03-06 14:20:07 -0500

    call the function make to create the vector

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@
 
 int main () {
 // unique= pointer that manages a raw pointer
-std::unique_ptr<std::vector<std::shared_ptr<Test>>> vec_ptr; // vec pointer is an object
-
+std::unique_ptr<std::vector<std::shared_ptr<Test>>> vec_ptr; // vec pointer is an object // creates object on the stack
+vec_ptr = make();// call the function make to make the pointer
 
 
 return 0;
