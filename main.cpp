a5fcee603ca0609c30242ff626eeaccbaee14387
BrianLevin <bml201095@gmail.com>
2021-03-06 14:39:51 -0500

    created the test class which is going to hold the test object values

--- main.cpp
+++ main.cpp
@@ -2,9 +2,20 @@
 #include <memory> // manages dynamic memory
 #include <vector>
 
+class Test {
+private:
+    int data;
+public:
+    Test() : data{0} { std::cout << "\tTest constructor (" << data << ")" << std::endl; }
+    Test(int data) : data {data} { std::cout << "\tTest constructor (" << data << ")" << std::endl; }
+    int get_data() const {return data; }
+    ~Test() {std::cout << "\tTest destructor (" << data << ")" << std::endl; }
+};
+
 // Function prototypes
-std::unique_ptr<std::vector<std::shared_ptr<Test>>> make();
+std::unique_ptr<std::vector<std::shared_ptr<Test>>> make(); // create unique pointer that points to the vector
 void fill(std::vector<std::shared_ptr<Test>> &vec, int num); // loop through elements
+void display(const std::vector<std::shared_ptr<Test>>&vec);  // displaying what ever is in the fector
 
 int main () {
 // unique= pointer that manages a raw pointer
@@ -15,6 +26,8 @@ vec_ptr = make();// call the function make to make the pointer
   int num;
     std::cin >> num;
  fill(*vec_ptr, num); // dereferance vec pointer when call fill
+
+  display(*vec_ptr);// dererenceing vector
 
 return 0;
 }
