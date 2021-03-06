7a25278f706fc0357375cd6d81308e4e0eea88f0
BrianLevin <bml201095@gmail.com>
2021-03-06 14:21:13 -0500

    created the function prototype for make() to make the vector

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include <memory> // manages dynamic memory
 #include <vector>
 
+// Function prototypes
+std::unique_ptr<std::vector<std::shared_ptr<Test>>> make();
+
 int main () {
 // unique= pointer that manages a raw pointer
 std::unique_ptr<std::vector<std::shared_ptr<Test>>> vec_ptr; // vec pointer is an object // creates object on the stack
